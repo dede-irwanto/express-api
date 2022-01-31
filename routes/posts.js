4a829f1e49dcd4c4dd33f464a8e8d5900a42c5fa
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-01-31 19:25:19 +0800

    install express-validator

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // import database
 const connection = require("../config/database");
 
-// index posts
+/**
+ * INDEX POSTS
+ */
 router.get("/", function (req, res) {
   // query
   connection.query(
@@ -25,5 +28,53 @@ router.get("/", function (req, res) {
     }
   );
 });
+
+/**
+ * STORE POST
+ */
+router.post(
+  "/store",
+  [
+    // validation
+    body("title").notEmpty(),
+    body("content").notEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+
+      // define form data
+      let formData = {
+        title: req.body.title,
+        content: req.body.content,
+      };
+
+      // insert query
+      connection.query(
+        "INSERT INTO posts SET ?",
+        formData,
+        function (err, rows) {
+          // throw error
+          if (err) {
+            return res.status(500).json({
+              status: false,
+              message: "Internal Server Error",
+            });
+          } else {
+            return res.status(200).json({
+              status: true,
+              message: "Insert Data Successfully",
+              data: rows[0],
+            });
+          }
+        }
+      );
+    }
+  }
+);
 
 module.exports = router;
